9266126809d51066fc22b75d8afc08d794432260
akankshaxa <dholwani.akanksha@gmail.com>
2026-02-17 10:00:17 +0530

    Added UC2 banner

--- banner.java
+++ banner.java
@@ -1,3 +1,5 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
 System.out.println("   ***   " + " " + " ****** "  + " " + "  ******   " + "  " ****  ");
 System.out.println(" **   ** " + " " + " **   ** " + " " + " **    **  " + "  **      ");
 System.out.println("**     **" + " " + " **    **" + " " + " **     ** " + "   **      ");
@@ -5,3 +7,5 @@ System.out.println("**     **" + " " + " **   ** " + " " + " **   **   " + "    
 System.out.println("**     **" + " " + " ** **   " + " " + " ** **     " + "       **   ");
 System.out.println(" **   **"  + " " + " **      " + " " + " **        " + "      **    ");
 System.out.println("   ***  "  + " " + " **      " + " " + " **        " + "   ****      ");
+	}
+}
